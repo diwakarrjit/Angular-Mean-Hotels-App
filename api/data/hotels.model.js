871c3e2d818e11bf11ba4a5a888fe2213418c31e
Jeffrey A McCommas <jeffmccommas@gmail.com>
2017-10-07 10:58:33 -0400

    eventSchema
    added new eventSchema to hotels.model

--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -20,6 +20,71 @@ var reviewSchema = new mongoose.Schema({
     "default" : Date.now
   }
 });
+var eventSchema = new mongoose.Schema({
+  eventName : {
+    type : String,
+    required : true,min : 10,
+    max : 20
+  },
+  Description: {
+    type : String,
+    
+  },
+  company : {
+    type : String,
+    required : true,min : 10,
+    max : 20
+  },
+  eventDate : {
+    type : Date,
+    required:true,
+    "default" : Date.now
+  },
+  email : {
+    type : String,
+    required:true,
+  },
+  capacity : {
+    type : Number,
+    required:true,
+    min : 1,
+    max : 20
+  },
+  rooms : {
+    type : Number,
+    required:true,
+    min : 1,
+    max : 20
+  },
+  phone : {
+    type : Number,
+    required:true,
+  },
+  inputAddress : {
+    type : String,
+    required:true,
+  },
+  phone : {
+    type : Number,
+    required:true,
+  },
+  inputCity : {
+    type : String,
+    required:true,
+  },
+  inputCity : {
+    type : String,
+    required:true,
+  },
+  inputState : {
+    type : String,
+    required:true,
+  },
+  inputZip : {
+    type : String,
+    required:true,
+  }
+});
 
 var roomSchema = new mongoose.Schema({
   type : String,
@@ -46,6 +111,7 @@ var hotelSchema = new mongoose.Schema({
   currency : String,
   reviews : [reviewSchema],
   rooms : [roomSchema],
+  events:[eventSchema],
   location : {
     address : String,
     // Always store coordinates longitude (East/West), latitude (North/South) order.
