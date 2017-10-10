45afff0cbaf93d731b1099fdcd1289335116883c
diwakarrjit <diwakarrjit@gmail.com>
2017-10-11 01:38:50 +0530

    register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {Http} from "@angular/http";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {DataService} from "../shared/data.service";
-import {ActivatedRoute, ParamMap} from "@angular/router";
-import {HotelService} from "../shared/hotel.service";
+import { Http } from "@angular/http";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { DataService } from "../shared/data.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { HotelService } from "../shared/hotel.service";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers:[DataService]
+  providers: [DataService]
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup
@@ -17,18 +17,20 @@ export class RegisterComponent implements OnInit {
   params: ParamMap
   hotelId: any;
   hotels: Array<any>;
+  errorMsg: string;
+  sucessMsg: string;
 
-  constructor(private dataService:DataService,private route:ActivatedRoute,private commonService:HotelService,) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private commonService: HotelService, ) { }
 
 
   ngOnInit() {
 
 
-    this.registerForm = new  FormGroup({
-      name: new  FormControl('', [Validators.required, Validators.minLength(8)]),
-      userName: new  FormControl('', [Validators.required, Validators.minLength(8)]),
-      password: new  FormControl('', [Validators.required, Validators.minLength(8)]),
-      passwordAgain: new  FormControl('', [Validators.required, Validators.minLength(8)])
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      userName: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      passwordAgain: new FormControl('', [Validators.required, Validators.minLength(8)])
     })
   }
   get name() {
@@ -51,27 +53,30 @@ export class RegisterComponent implements OnInit {
 
 
 
-  register =  function () {
+  register = function () {
     let hotel = this
 
     var user = {
-      username: hotel.username,
-      password: hotel.password
+      username: hotel.userName.value,
+      password: hotel.password.value
     };
-    if (!hotel.username || !hotel.password){
-      hotel.error = 'Please add a username and a password.';
+
+    let body : string = JSON.stringify({ "username" : "hotel.username", "password" : "hotel.password"});
+    if (!hotel.userName.value || !hotel.password.value) {
+      this.errorMsg = 'Please add a username and a password.';
 
     } else {
-      if (hotel.password !== hotel.passwordAgain) {
-        hotel.error = 'Please make sure the passwords match.';
+      if (hotel.password.value !== hotel.passwordAgain.value) {
+        this.errorMsg = 'Please make sure the passwords match.';
       } else {
-        this.http.post('/api/users/register', user).then(function(result) {
-          console.log(result);
-          let hotelmessage ='Successful registration, please login';
-          hotel.error = '';
-        }).catch(function(error) {
-          console.log(error);
-        });
+        this.dataService.registerUser(body).subscribe(x=> console.log(x));
+        // this.http.post('/api/users/register', user).then(function(result) {
+        //   console.log(result);
+        //   let hotelmessage ='Successful registration, please login';
+        //   hotel.error = '';
+        // }).catch(function(error) {
+        //   console.log(error);
+        // });
       }
     }
   }
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { Http, Headers, Response,RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DataService {
 
   result:any;
-
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private options = new RequestOptions({ headers: this.headers });
   constructor(private _http: Http) { }
 
   gethotels() {
@@ -14,6 +16,10 @@ export class DataService {
   }
   gethotelInfo(id:any) {
     return this._http.get('http://localhost:3000/api/hotels/' + id).map(res => res.json());
+  }
+
+  registerUser(user){
+    return this._http.post('http://localhost:3000/api/users/register',user, this.options).subscribe(res => res.json());
   }
 
 }
